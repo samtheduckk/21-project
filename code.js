0653e203215db7283c4bc0243f2d86b942a0bf49
DanielCRosa <danielcasarindarosa@gmail.com>
2023-01-27 23:38:52 -0300

    fixedAsideButton

--- code.js
+++ code.js
@@ -1,4 +1,14 @@
+let menuActive = false;
+
 function openMenu(){
+    if(menuActive == false){
     let menu = document.querySelector('aside')
     menu.style.flex = '0';
+    menuActive = true;
+}else if (menuActive == true){
+    let menu = document.querySelector('aside')
+    menu.style.flex = '30%';
+    menuActive = false
+}
+
 }
