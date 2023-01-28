2384e23a2eb1527a0a80d0b75025d19c78955881
DanielCRosa <danielcasarindarosa@gmail.com>
2023-01-28 16:53:39 -0300

    firstPHPFilesUpdate

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+    
+    
+    define('HOST','127.0.0.1');
+    define('USUARIO','root');
+    define('SENHA', '');
+    define('DB','database');
+    $conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+
+    
+?>
